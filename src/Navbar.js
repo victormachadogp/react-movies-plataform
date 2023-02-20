6794d7b553fc06430e25917fa48b9931ff047e14
Victor Machado <victormachadogp@gmail.com>
2023-02-20 00:25:55 -0300

    Adding navbar written content

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,12 @@
 const NavBar = () => {
     return (
-        <nav className="bg-gray-800">
-            <p className="uppercase text-white logo">Shaun <span className="font-bold text-pink-500">Movies</span></p>
+        <nav className="flex items-center justify-between">
+            <p className="logo uppercase my-8">Shaun<span className="font-bold text-pink-500">Movies</span></p>
+            <div className="flex uppercase space-x-4">
+                <a href='#'>Início</a>
+                <a href='#'>Catálogo</a>
+                <div>Icon</div>
+            </div>
         </nav>
     )
 }
