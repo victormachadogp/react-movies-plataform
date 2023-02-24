8a73367d73350089f47593ce6abc2a304cf0f4f4
Victor Machado <victormachadogp@gmail.com>
2023-02-24 17:19:31 -0300

    Creating a expanded-width CSS class

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,7 +1,9 @@
 const Carousel = () => {
     return (
-        <section className="carousel">
-            <h2 class='title uppercase'><span className="font-bold">Lançamentos</span> da Semana</h2>
+        <section className="expanded-width carousel">
+            <div className="max-w-5xl 2xl:max-w-6xl mx-auto">
+                <h2 class='title uppercase'><span className="font-bold">Lançamentos</span> da Semana</h2>
+            </div>
         </section>
     )
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,6 @@
 const NavBar = () => {
     return (
-        <nav className="">
+        <nav className="expanded-width">
             <div className="max-w-5xl 2xl:max-w-6xl mx-auto flex items-center justify-between">
                 <p className="logo uppercase my-8">Shaun<span className="font-bold text-pink-500">Movies</span></p>
                 <div className="flex uppercase space-x-4">
