d2e5c828bbac29934c4aba206b955aa9f494bdeb
Victor Machado <victormachadogp@gmail.com>
2023-02-24 16:44:53 -0300

    Expanded-width added inside navbar

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import Navbar from './Navbar';
 function App() {
   return (
     <div className="App">
+      <section className='overflow-hidden main-container mx-auto'>
+        <div className='max-w-5xl 2xl:max-w-6xl mx-auto'>
+
       <Navbar />
+        </div>
+      </section>
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,13 @@
 const NavBar = () => {
     return (
-        <nav className="flex items-center justify-between">
-            <p className="logo uppercase my-8">Shaun<span className="font-bold text-pink-500">Movies</span></p>
-            <div className="flex uppercase space-x-4">
-                <a href='#'>Início</a>
-                <a href='#'>Catálogo</a>
-                <div>Icon</div>
+        <nav className="">
+            <div className="max-w-5xl 2xl:max-w-6xl mx-auto flex items-center justify-between">
+                <p className="logo uppercase my-8">Shaun<span className="font-bold text-pink-500">Movies</span></p>
+                <div className="flex uppercase space-x-4">
+                    <a href='#'>Início</a>
+                    <a href='#'>Catálogo</a>
+                    <div>Icon</div>
+                </div>
             </div>
         </nav>
     )
