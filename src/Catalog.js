fccd41fdd6dd815886651b050e0cf4ec16642d84
Victor Machado <victormachadogp@gmail.com>
2023-02-25 16:33:22 -0300

    Catalog - Buttons style added

--- src/Catalog.js
+++ src/Catalog.js
@@ -1,9 +1,20 @@
 const Catalog = () => {
     return (
-        <section className="expanded-width catalog-title">
-            <div className="max-w-5xl 2xl:max-w-6xl mx-auto flex items-center justify-between">
-                <h2 className="title"> <span className="font-bold">Catálogo</span> Completo</h2>
+        <section>
+            <div className="expanded-width catalog-title">
+                <div className="max-w-5xl 2xl:max-w-6xl mx-auto flex items-center justify-between">
+                    <h2 className="title"> <span className="font-bold">Catálogo</span> Completo</h2> 
+                </div>
             </div>
+             <div>
+                <div className="flex justify-between">
+                    <div>
+                        <button className="btn-primary">por gênero</button>
+                        <button className="btn-secondary ml-4">mais populares</button>
+                    </div>
+                    <button className="btn-primary">Lista</button>
+                </div>
+             </div>
         </section>
     )
 }
