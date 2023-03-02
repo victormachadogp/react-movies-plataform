52cad6d9da67ca227aae246447337d42abfbc31e
Victor Machado <victormachadogp@gmail.com>
2023-03-02 12:32:16 -0300

    NavLink added to Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,14 @@
+import { NavLink } from "react-router-dom"
+
+
 const NavBar = () => {
     return (
         <nav className="expanded-width">
             <div className="max-w-5xl 2xl:max-w-6xl mx-auto flex items-center justify-between">
                 <p className="logo uppercase my-8">Shaun<span className="font-bold text-pink-500">Movies</span></p>
                 <div className="flex uppercase space-x-4">
-                    <a href='#'>Início</a>
-                    <a href='#'>Catálogo</a>
+                    <NavLink to="/">Início</NavLink>
+                    <NavLink to="movie-details">Catálogo</NavLink>
                     <div>Icon</div>
                 </div>
             </div>
