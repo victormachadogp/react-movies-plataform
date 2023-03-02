dc52d0fe50160e518aa95377c74860c6484ba6ba
Victor Machado <victormachadogp@gmail.com>
2023-03-02 13:31:52 -0300

    Adding border bottom to active element

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,11 +5,11 @@ const NavBar = () => {
     return (
         <nav className="expanded-width">
             <div className="max-w-5xl 2xl:max-w-6xl mx-auto flex items-center justify-between">
-                <p className="logo uppercase my-8">Shaun<span className="font-bold text-pink-500">Movies</span></p>
+                <p className="logo uppercase">Shaun<span className="font-bold text-pink-500">Movies</span></p>
                 <div className="flex uppercase space-x-4">
-                    <NavLink to="/">Início</NavLink>
-                    <NavLink to="movie-details">Catálogo</NavLink>
-                    <div>Icon</div>
+                    <NavLink className="py-10" to="/">Início</NavLink>
+                    <NavLink className="py-10" to="movie-details">Catálogo</NavLink>
+                    <div className="py-10">Icon</div>
                 </div>
             </div>
         </nav>
