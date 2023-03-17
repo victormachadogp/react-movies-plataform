405018fe38486c5ac668fcf6928dbe2598ad6da5
Victor Machado <victormachadogp@gmail.com>
2023-03-17 11:48:53 -0300

    Toggle layout display feature

--- src/Catalog.js
+++ src/Catalog.js
@@ -1,6 +1,9 @@
+import { useState } from "react"
 import FetchData from "./FetchData"
 
 const Catalog = () => {
+    const [buttonText, setButtonText] = useState("em lista");
+      
     const {data: movieData} = FetchData('https://api.themoviedb.org/3/movie/popular?api_key=13bed307564b94b94af8c359e589d92e&language=en-US&page=1')
     const {data: genreData} = FetchData('https://api.themoviedb.org/3/genre/movie/list?api_key=13bed307564b94b94af8c359e589d92e&language=pt-BR')
 
@@ -17,6 +20,31 @@ const Catalog = () => {
         return genreNames.join(", ")
     }
 
+    const changeFlexLayout = () => {
+        const catalogBlock = document.querySelector(".catalog-block");
+        catalogBlock.classList.toggle("flex-col");
+        catalogBlock.classList.toggle("w-full");
+
+        const elements = document.querySelectorAll(".catalog-element");
+        elements.forEach((element) => {
+
+            if(element.style.width === "100%") {
+                element.style.width = "495px"
+            } else {
+                element.style.width = "100%"
+            }
+            
+        })
+
+
+        buttonText ? setButtonText("em grid") : setButtonText("em lista") 
+        if (buttonText === "em lista") {
+            setButtonText("em grid");
+          } else {
+            setButtonText("em lista");
+          }
+    }
+
 
     return (
         <section>
@@ -31,12 +59,12 @@ const Catalog = () => {
                         <button className="btn-primary">por gênero</button>
                         <button className="btn-secondary ml-4">mais populares</button>
                     </div>
-                    <button className="btn-primary">em lista</button>
+                    <button onClick={changeFlexLayout} className="btn-primary">{buttonText}</button>
                 </div>
                 
                 {/* To change view just remove/add the flex-col and w-full classes */}
                     {movieData && 
-                        <div className="flex gap-2 flex-wrap">
+                        <div className="flex gap-2 flex-wrap catalog-block">
                             {movieData.results.slice(0,6).map((item) => {
                                 return <div className="catalog-element flex" key={item.id}>
                                            <img className="catalog-movie-img" src={`http://image.tmdb.org/t/p/w300/${item.poster_path}`} alt={item.title}/>
