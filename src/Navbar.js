95d8b2a87e635084636a4af7a5a209b7fed1fdec
Victor Machado <victormachadogp@gmail.com>
2023-03-22 14:47:12 -0300

    Search icon added

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,12 @@
 import { NavLink } from "react-router-dom"
 
 
+const handleSearch = () => {
+    const searchBlock = document.querySelector(".search-box");
+
+    console.log(searchBlock)
+}
+
 const NavBar = () => {
     return (
         <nav className="expanded-width">
@@ -9,7 +15,27 @@ const NavBar = () => {
                 <div className="flex uppercase space-x-4">
                     <NavLink className="py-10" to="/">Início</NavLink>
                     <NavLink className="py-10" to="movie-details">Catálogo</NavLink>
-                    <div className="py-10">Icon</div>
+                    <div onClick={handleSearch} className="py-10">
+                    <svg width="22" height="22" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g clipPath="url(#clip0_1586_138)">
+                        <path d="M9.98598 0.922852C8.19352 0.922852 6.4413 1.45438 4.95092 2.45022C3.46053 3.44607 2.29892 4.86149 1.61297 6.51752C0.927022 8.17355 0.747547 9.99579 1.09724 11.7538C1.44693 13.5118 2.31009 15.1267 3.57756 16.3942C4.84502 17.6616 6.45988 18.5248 8.2179 18.8745C9.97593 19.2242 11.7982 19.0447 13.4542 18.3587C15.1102 17.6728 16.5257 16.5112 17.5215 15.0208C18.5173 13.5304 19.0489 11.7782 19.0489 9.98574C19.0487 7.58216 18.0938 5.27707 16.3942 3.57748C14.6947 1.87789 12.3896 0.923005 9.98598 0.922852V0.922852Z" stroke="#EAEAEA" strokeWidth="2.30769" strokeMiterlimit="10"/>
+                        <path d="M16.7476 16.7476L23.077 23.077" stroke="#EAEAEA" strokeWidth="2.30769" strokeMiterlimit="10" strokeLinecap="round"/>
+                        </g>
+                        <defs>
+                        <clipPath id="clip0_1586_138">
+                        <rect width="24" height="24" fill="white"/>
+                        </clipPath>
+                        </defs>
+                    </svg>
+                    </div>
+                </div>
+            </div>
+            <div className="search-box">
+                <div className="max-w-5xl 2xl:max-w-6xl mx-auto">
+                    <input className="w-full"/>
+                    <div className="movie-list pb-20">
+
+                    </div>
                 </div>
             </div>
         </nav>
