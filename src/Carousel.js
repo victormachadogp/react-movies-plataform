e9eadc7e4255cd725a9fcd85b7cad43c2177fdb1
Victor Machado <victormachadogp@gmail.com>
2023-04-22 12:00:04 -0300

    Carousel counter removed from html

--- src/Carousel.js
+++ src/Carousel.js
@@ -94,8 +94,6 @@ const Carousel = () => {
                     </div>
                 </div>
 
-                <p>{counter}</p>
-
                 <button className="prev-button" onClick={prevImage}>
                     <svg width="25" height="42" viewBox="0 0 25 42" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <g clip-path="url(#clip0_1586_125)">
